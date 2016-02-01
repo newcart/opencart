4006ad9297c4cb289822f8f493124531cf5632fd
kallef <prhost@prhost.com.br>
2016-02-01 21:13:36 -0200

    mais organizacao e alguns ajustes do core

--- constants.php
+++ constants.php
@@ -1,20 +1,42 @@
 <?php
 
+define('IS_ADMIN', basename(realpath('')) == 'admin' ? true : false);
+define('ENVIRONMENT', IS_ADMIN ? 'admin' : 'catalog');
+
+//get domain name
+define('DOMAINNAME', isset($_SERVER['SERVER_NAME']) ? $_SERVER['SERVER_NAME'] : null);
+
+//define core directory
+define('DIR_CORE', realpath(DIR_ROOT . '/core/'));
+
+$base_url = DOMAINNAME . $_SERVER['SCRIPT_NAME'];
+$base_url = str_replace(['/core/admin/', '/core', '/index.php'], '/', $base_url);
+define('BASEURL', $base_url);
+
 // HTTP
-define('HTTP_SERVER', 'http://sandbox.dev/prhost/opencommerce/core/');
+define('HTTP_SERVER', 'http://' . BASEURL . 'core/admin/');
+define('HTTP_CATALOG', 'http://' . BASEURL);
 
 // HTTPS
-define('HTTPS_SERVER', 'https://sandbox.dev/prhost/opencommerce/core/');
+define('HTTPS_SERVER', 'https://' . BASEURL . 'core/admin/');
+define('HTTPS_CATALOG', 'http://' . BASEURL);
 
 // DIR
-define('DIR_APPLICATION', '/var/www/sandbox.dev/prhost/opencommerce/core/catalog/');
-define('DIR_SYSTEM', '/var/www/sandbox.dev/prhost/opencommerce/core/system/');
-define('DIR_LANGUAGE', '/var/www/sandbox.dev/prhost/opencommerce/core/catalog/language/');
-define('DIR_TEMPLATE', '/var/www/sandbox.dev/prhost/opencommerce/core/catalog/view/theme/');
-define('DIR_CONFIG', '/var/www/sandbox.dev/prhost/opencommerce/core/system/config/');
-define('DIR_IMAGE', '/var/www/sandbox.dev/prhost/opencommerce/core/image/');
-define('DIR_CACHE', '/var/www/sandbox.dev/prhost/opencommerce/core/system/cache/');
-define('DIR_DOWNLOAD', '/var/www/sandbox.dev/prhost/opencommerce/core/system/download/');
-define('DIR_UPLOAD', '/var/www/sandbox.dev/prhost/opencommerce/core/system/upload/');
-define('DIR_MODIFICATION', '/var/www/sandbox.dev/prhost/opencommerce/core/system/modification/');
-define('DIR_LOGS', '/var/www/sandbox.dev/prhost/opencommerce/core/system/logs/');
+if(IS_ADMIN) {
+    define('DIR_TEMPLATE', DIR_CORE . '/' . ENVIRONMENT . '/view/template/');
+} else {
+    define('DIR_TEMPLATE', DIR_ROOT . '/theme/');
+}
+
+define('DIR_APPLICATION', DIR_CORE . '/' . ENVIRONMENT . '/');
+define('DIR_SYSTEM', DIR_CORE . '/system/');
+define('DIR_STORAGE', DIR_ROOT . '/storage');
+define('DIR_LANGUAGE', DIR_CORE . '/' . ENVIRONMENT . '/language/');
+define('DIR_CONFIG', DIR_CORE . '/config/');
+define('DIR_IMAGE', DIR_STORAGE . '/image/');
+define('DIR_CACHE', DIR_STORAGE . '/cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . '/download/');
+define('DIR_UPLOAD', DIR_STORAGE . '/upload/');
+define('DIR_MODIFICATION', DIR_STORAGE . '/modification/');
+define('DIR_LOGS', DIR_STORAGE . '/logs/');
+define('DIR_CATALOG', DIR_CORE . '/catalog/');
